f741560401c66723f773e7ec3eb6bc40a01e537f
eastshine980 <eastshine980@gmail.com>
2022-04-19 18:15:48 +0900

    feat : 메세지 추가 (#1)
    내가 작업한 내용 추가 함 test1
    
    해결 #1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("test3 test add ");
+        System.out.println("test1 test add ");
     }
 }
